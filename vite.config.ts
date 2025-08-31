4a2f92c495f18bb279f2d31af2977caf55f976ca
mxlle <almut.m.f@gmail.com>
2025-08-31 14:43:55 +0200

    build: fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig(({ mode, command }) => {
   const getCssIdentifier = memoize(idGenerator(), 2);
 
   return {
+    base: "",
     envPrefix: ["GERMAN_ENABLED", "POKI_ENABLED"],
     build: {
       minify: production ? "terser" : false,
